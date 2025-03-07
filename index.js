ee02ce406f99f1c514c61b70775e7c34e984103a
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-03-07 16:58:30 -0300

    Commit correto

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const form = document.getElementById('formList') //Pegando o form
 const inputAdd = document.getElementById('inputAdd') //Pegando o input pra adicionar tarefas
-const inputSearch = document.getElementById('inputSearch')//Pegando o input para pesquisar tarefas depois
+const inputSearch = document.getElementById('inputSearch')//Pegando o input para pesquisar tarefas depoiss
+
