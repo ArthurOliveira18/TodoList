7e9f403a8d4c4bb36319e19b36c12dc989215af4
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-03-07 16:55:15 -0300

    teste'

---
+++ index.js
@@ -0,0 +1,3 @@
+const form = document.getElementById('formList') //Pegando o form
+const inputAdd = document.getElementById('inputAdd') //Pegando o input pra adicionar tarefas
+const inputSearch = document.getElementById('inputSearch')//Pegando o input para pesquisar tarefas depois
