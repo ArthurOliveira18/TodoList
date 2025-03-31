43b1f97a71615582214fe9c07f0282497e015e3e
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-03-31 12:02:22 -0300

    finalizando algumas alteraçoes'

--- index.js
+++ index.js
@@ -11,31 +11,80 @@ form.addEventListener('submit', function (ev) {
     const valorAll = lista.options[0].value
     const valorPending = lista.options[1].value
     const valorComplete = lista.options[2].value
-
+    
 
 
 })
-// Criando o id para excluir apenas aquela tarefa com o id especifo.
-let taskId = 1;  // Variável para contar as tarefas
+
+
+// Aqui eu estou pegando o valor do array "tasks"
+const tasksString = localStorage.getItem("tasks");
+
+// Logo aqui em baixo eu estou conferindo se tem algum conteudo dentro do array "tasks" Caso n tenha, crie um array chamado "tasks"
+let tasks;
+
+if (!tasksString || tasksString === "null" || tasksString === "") {
+    tasks = [];
+} else {
+    tasks = JSON.parse(tasksString);
+}
+
+
+// Define o último ID salvo ou inicia em 1
+let taskId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+
 
 function addTask() {
-
-    console.log(inputAdd.value)
-
     if (inputAdd.value == "") {
         alert("O campo de adicionar task está vazio")
     } else {
-        const li = document.createElement('li')
-        li.textContent = inputAdd.value
 
-        const taskList = document.getElementById('taskList')
-        taskList.appendChild(li)
+
+        // Criando a estrutura para salvar o ID e o texto do usuario
+        const task = {
+            id:taskId,
+            text: inputAdd.value
+        }
+        // Toda vez que clico para add tarefa, ele incrementa mais um no ID e depois salva em um array
+        taskId++
+        tasks.push(task)
+
+        //Salvando o array tasks no local storage 
+        localStorage.setItem("tasks" , JSON.stringify(tasks))
+        
+        
+        const infoL = localStorage.getItem("tasks")
+        console.log(infoL)
+        
+
+        renderTasks();
 
         inputAdd.value = ""
+
+       
+        
     }
+
+    renderTasks();
 
 
 }
+
+// Função para mostrar na tela oque está armazenado no Local Storage
+function renderTasks() {
+    const taskList = document.getElementById('taskList')
+    //  Limpa a lista para evitar duplicação
+    taskList.innerHTML = "";
+
+    // Iterar sobre cada elemento da lista e pegar cada elemento e mostrar na tela
+    tasks.forEach(task => {
+        const li = document.createElement('li')
+        li.textContent = `${task.id} : ${task.text}`
+        taskList.appendChild(li)
+    });
+}
+
+
 
 
 
